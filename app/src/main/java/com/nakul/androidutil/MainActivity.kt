04a96472b51f0ac9fd787c2d0f5574c1c834c650
Nakul <nakul@apptunix.com>
2023-08-28 13:48:43 +0530

    Basic Setup

---
+++ app/src/main/java/com/nakul/androidutil/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nakul.androidutil
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nakul.androidutil.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
